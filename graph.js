585cfc8989212580798343abdf74c035f27d8281
ash1day <y4ashida@gmail.com>
2016-09-12 03:39:18 +0900

    jsファイルを分割

---
+++ graph.js
@@ -0,0 +1,90 @@
+function build_graph(json) {
+  var nodes = [], links = []
+
+  // 潜在変数の定義式より、ノードとリンクを作成
+  for (var left_var in json.latent_variables) {
+    nodes.push({ id: left_var, group: 1 })
+    for (right_var of json.latent_variables[left_var]) {
+      nodes.push({ id: right_var.name, group: 2 })
+      links.push({ source: left_var, target: right_var.name, value: right_var['Estimate'], p: right_var['P(>|z|)'] })
+    }
+  }
+
+  // 回帰の式からリンクを作成
+  for (var left_var in json.regressions) {
+    for (right_var of json.regressions[left_var]) {
+      links.push({ source: left_var, target: right_var.name, value: right_var['Estimate'], p: right_var['P(>|z|)'] })
+    }
+  }
+
+  return {
+    nodes: nodes,
+    links: links
+  }
+}
+
+function graph(json) {
+  var graph = build_graph(json)
+
+  var svg = d3.select('svg'),
+      width = +svg.attr('width'),
+      height = +svg.attr('height')
+
+  // TODO: いい感じの矢印に
+  svg.append('defs').append('marker')
+    .attr('id', 'arrowhead')
+    .attr('refX', 4.5)
+    .attr('refY', 2)
+    .attr('markerWidth', 5)
+    .attr('markerHeight', 4)
+    .attr('orient', 'auto')
+    .append('path')
+        .attr('d', 'M 0,0 V 4 L3,2 Z')
+        .attr('fill', 'steelblue')
+
+  var color = d3.scaleOrdinal(d3.schemeCategory20)
+
+  var simulation = d3.forceSimulation()
+      .force('link', d3.forceLink().distance(50).id(function(d) { return d.id }))
+      .force('charge', d3.forceManyBody())
+      .force('center', d3.forceCenter(width / 2, height / 2))
+
+  var link = svg.append('g')
+      .attr('class', 'links')
+    .selectAll('line')
+    .data(graph.links)
+    .enter().append('line')
+      .attr('marker-end', 'url(#arrowhead)')
+      .attr('stroke-width', function(d) { return Math.sqrt(d.value * 10) })
+      .attr('stroke-opacity', function(d) { return 1 - d.p })
+
+  var node = svg.selectAll('.node')
+      .data(graph.nodes)
+    .enter().append('g')
+      .attr('class', 'node')
+
+  node.append('circle')
+      .attr('r', 6)
+      .attr('fill', function(d) { return color(d.group) })
+
+  node.append('text')
+      .attr('dx', 12)
+      .attr('dy', '.35em')
+      .text(function(d) { return d.id })
+
+  simulation
+      .nodes(graph.nodes)
+      .on('tick', ticked)
+
+  simulation.force('link')
+      .links(graph.links)
+
+  function ticked() {
+    link.attr('x1', function(d) { return d.source.x })
+        .attr('y1', function(d) { return d.source.y })
+        .attr('x2', function(d) { return d.target.x })
+        .attr('y2', function(d) { return d.target.y })
+
+    node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')' })
+  }
+}
---
+++ heatmap.js
@@ -0,0 +1,67 @@
+function covariances_matrix(json) {
+  raw_matrix = {}
+
+  names = json.names.sort()
+  len   = names.length
+
+  for (var row_name of json.names) {
+    raw_matrix[row_name] = {}
+    for (var column_name of json.names) {
+      raw_matrix[row_name][column_name] = 0 // 0でいいのか?
+    }
+
+    // 分散
+    raw_matrix[row_name][row_name] = json.variances[row_name].Estimate
+  }
+
+  // 共分散
+  for (var row_name in json.covariances) {
+    for (co_obj of json.covariances[row_name]) {
+      raw_matrix[row_name][co_obj.name] = co_obj.Estimate
+      raw_matrix[co_obj.name][row_name] = co_obj.Estimate
+    }
+  }
+
+  matrix = []
+
+  for (i = 0; i < len; i++) {
+    matrix[i] = []
+    for (j = 0; j < len; j++) {
+      matrix[i][j] = raw_matrix[names[i]][names[j]]
+    }
+  }
+
+  return { names: names, matrix: matrix }
+}
+
+function heatmap(data, id){
+  names = data.names
+  matrix = data.matrix
+
+  var data = [
+    {
+      x: names,
+      y: names.concat().reverse(),
+      z: matrix.reverse(),
+      type: 'heatmap'
+    }
+  ]
+
+  var layout = {
+    title: 'Covariances matrix',
+    annotations: [],
+    xaxis: {
+      ticks: '',
+      side: 'top'
+    },
+    yaxis: {
+      ticks: ' ',
+      ticksuffix: ' ',
+      autosize: false
+    },
+    width: 400,
+    height: 400
+  }
+
+  Plotly.newPlot(id, data, layout);
+}
